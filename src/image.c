e68f62d88a9742310e64ee5900387e72c3de374b
Jasmine Ngo <158764381+jasxlynn@users.noreply.github.com>
2024-10-23 22:39:57 +0000

    check pt1

--- src/image.c
+++ src/image.c
@@ -9,82 +9,62 @@ Image *load_image(char *filename) {
         exit(EXIT_FAILURE);
     }
 
-    Image *img = malloc(sizeof(Image));
-
     //check if valid PPM file
     char buffer[100];
-    char format_variant[100];
-    unsigned int width, height = 0; 
+    char format_variant[2];
+    unsigned int width, height; 
 
-    if(fgets(buffer, sizeof(buffer), fp) != NULL){
-        sscanf(buffer, "%s", format_variant);
-    }
+    fgets(buffer, sizeof(buffer), fp);
+    sscanf(buffer, "%s", format_variant);
     
-    while(fgets(buffer, sizeof(buffer), fp) != NULL){
-        if(buffer[0] == '#'){
-            continue;
-        }
-        if(sscanf(buffer, "%u %u", &width, &height)){
-            break; 
-        }
+    if(strcmp(format_variant, "P3") != 0){
+        ERROR("Invalid file type");
+        exit(EXIT_FAILURE); 
+    }   
+    fgets(buffer, sizeof(buffer), fp);
+    while(buffer[0] == '#'){
+        fgets(buffer, sizeof(buffer), fp);
     }
+
+    sscanf(buffer, "%u %u", &width, &height); 
+    fgets(buffer, sizeof(buffer),fp);
 
     Image *img = malloc(sizeof(Image));
+
     img->width = width;
     img->height = height; 
-    // unsigned int width, height = 0;
-    // char line[MAX_SIZE]; 
-    // //skip comment line and read width and height 
-    // // while(fgets(line,sizeof(line), fp)){
-    // //     if(strcmp(line[0], "#") == 0){
-    // //         continue;
-    // //     }
-    // //     sscanf(line, "%u %u", &width, &height);
-    // //     break;
-    // // }
-    // unsigned int max_value = 0;
-    // fscanf(fp, "%u", &max_value);
+    img->pixels = malloc(width * height * sizeof(unsigned int)); 
 
-    // //add data into struct 
-    // img->width = width;
-    // img->height = height;
-    // img->pixels = malloc(width * height * 3);
-
-    // for(unsigned int i = 0; i < width * height; i++){
-    //     unsigned int r, g, b;
-    //     fscanf(fp, "%u %u %u", &r, &g, &b);
-    //     img->pixels[i * 3] = (unsigned char)r;  
-    //     img->pixels[i * 3 + 1] = (unsigned char)g;
-    //     img->pixels[i * 3 + 2] = (unsigned char)b;
-    // }
+    for(unsigned int i = 0; i < width * height; i++){
+        unsigned int r, g, b; 
+        fscanf(fp, "%u %u %u", &r, &g, &b);
+        img->pixels[i * 3] = r;
+        img->pixels[i * 3 + 1] = g;
+        img->pixels[i * 3 + 2] = b;
+    }
 
     fclose(fp);
     return img;
 }
 
 void delete_image(Image *image) {
-    (void)image;
-    // if(image != NULL){
-    //     free(image->pixels);
-    //     free(image);
-    // }
+    if(image != NULL){
+        free(image->pixels);
+        free(image);
+    }
 }
 
 unsigned short get_image_width(Image *image) {
-    (void)image;
-    return 0;
+    return image->width;
 }
 
 unsigned short get_image_height(Image *image) {
-    (void)image;
-    return 0;
+    return image->height;
 }
 
 unsigned char get_image_intensity(Image *image, unsigned int row, unsigned int col) {
-    (void)image;
-    (void)row;
-    (void)col;
-    return 0;
+    printf("%u\n", image->pixels[row*col]);
+    return image->pixels[row*col];
 }
 
 unsigned int hide_message(char *message, char *input_filename, char *output_filename) {
